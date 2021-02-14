3303809656401d5c3fa5ce8772b4579f1a248e73
Andy Loftus <aloftus@gmail.com>
2021-02-14 14:53:10 -0600

    save config vars in a file

--- setup.sh
+++ setup.sh
@@ -11,16 +11,27 @@
 BASE=$( dirname $0 )
 TS=$(date +%s)
 
+save_config_var() {
+  local _fn_conf="$INSTALL_DIR/.setup.cfg"
+  local _key="$1"
+  local _val="${!_key}"
+  [[ -e "$_fn_conf" ]] || echo "# last update" > "$_fn_conf"
+  sed -i -n -e "/last update/c# last update $TS" "$_fn_conf"
+  sed -i -n -e "/^$_key=/!p" -e "\$a$_key=$_val" "$_fn_conf"
+}
+
 # Output from 'pwd' will be the directory from which quickstart was invoked
 INSTALL_DIR="$(pwd)"
+[[ -n "$QWIKTEST_INSTALL_DIR" ]] && INSTALL_DIR="$QWIKTEST_INSTALL_DIR"
 
 echo "Hello from '$BASE/setup.sh'"
 echo "Install dir: '$INSTALL_DIR'"
 
 GIT_URL=$( git -C "${BASE}" remote get-url origin )
+save_config_var GIT_URL
 GIT_BRANCH=$( git -C "${BASE}" branch | awk '/^\*/ {print $NF}' )
-echo "URL: $GIT_URL"
-echo "BRANCH: $GIT_BRANCH"
+save_config_var GIT_BRANCH
 
+cat "$INSTALL_DIR/.setup.cfg"
 
 echo "Nothing else to do."
